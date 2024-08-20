feac691ca533839088b89704ba9332f253f7c7d0
nandini <ramavath.nandini0026@gmail.com>
2024-08-20 23:24:31 +0530

    first  commit

---
+++ portfolio/index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typed", {
+    strings: ["FrontEnd Developer", "Web Developer", "Java Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
